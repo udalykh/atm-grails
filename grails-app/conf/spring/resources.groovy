1537f4ceb1ab5e970eed74581af0b245ecf6b17b
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-20 15:06:24 +0800

    Grails refactoring

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,10 @@
 // Place your Spring DSL code here
+package spring
+
+import com.test.atm.AtmCommandFactory
+import com.test.atm.MoneyStorage
+
 beans = {
+    commandBean(AtmCommandFactory)
+    moneyStorageBean(MoneyStorage)
 }
