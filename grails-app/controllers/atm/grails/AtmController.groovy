4d696e380fdb6db76f699b80fec88fa61652536b
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-21 12:10:54 +0800

    Grails refactoring

--- grails-app/controllers/atm/grails/AtmController.groovy
+++ grails-app/controllers/atm/grails/AtmController.groovy
@@ -1,9 +1,53 @@
 package atm.grails
 
+import com.test.atm.*
 
 class AtmController {
 
+    MoneyStorage moneyStorage
+    AtmCommandFactory atmCommandFactory
+
+    def commandBean
+    def moneyStorageBean
+
+    Map runCommand(String command, String[] arguments) {
+        try {
+            AtmCommand atmCommand = atmCommandFactory.create(command)
+            atmCommand.execute(arguments)
+        } catch (ignore) {
+            println('ERROR')
+        }
+        null
+    }
+
     def index() {
-        println ""
+        try {
+            String command = ''
+            String currency = ''
+            String value = ''
+            String number = ''
+            String amount = ''
+            String[] arguments = []
+
+            switch (CommandType.getCommandType(command)) {
+                case CommandType.REMAININGS:
+                    arguments = []
+                    break
+                case CommandType.ADD:
+                    arguments = [currency, value, number]
+                    break
+                case CommandType.WITHDRAW:
+                    arguments = [currency, amount]
+                    break
+                default:
+                    arguments = []
+            }
+
+            Map<BankNote, Integer> response = runCommand(command, arguments)
+            [response: response.entrySet()]
+        } catch (AtmStateException e) {
+            log.error("Error while ATM operation: ${e.message}")
+            [error: true]
+        }
     }
 }
