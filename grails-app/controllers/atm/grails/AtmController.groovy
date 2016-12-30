b14b8cf7a513b4fba2ac9ae0c3f49edf7b1ce79a
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-30 23:46:41 +0800

    Grails refactoring

--- grails-app/controllers/atm/grails/AtmController.groovy
+++ grails-app/controllers/atm/grails/AtmController.groovy
@@ -12,29 +12,18 @@ class AtmController {
         String value = params["value"]
         String number = params["number"]
         String amount = params["amount"]
-        boolean boolA
-        boolean boolB
-        boolean boolC
+
         Map<BankNote, Integer> response = [:]
         try {
             switch (CommandType.getCommandType(command)) {
                 case CommandType.REMAININGS:
                     response = new RequestRemainings(moneyStorage).execute()
-                    boolA = true
-                    boolB = true
-                    boolC = true
                     break
                 case CommandType.ADD:
                     response = new DepositCommand(moneyStorage).execute(currency, value, number)
-                    boolA = false
-                    boolB = false
-                    boolC = true
                     break
                 case CommandType.WITHDRAW:
                     response = new WithdrawalCommand(moneyStorage).execute(currency, amount)
-                    boolA = true
-                    boolB = true
-                    boolC = false
                     break
             }
             if (!response) {
