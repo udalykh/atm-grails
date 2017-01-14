692f3f80b5ba61a6d44375b6c2e995b9ba503293
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-14 21:24:23 +0800

    Grails refactoring

--- grails-app/domain/atm/grails/MoneyDomain.groovy
+++ grails-app/domain/atm/grails/MoneyDomain.groovy
@@ -1,11 +1,21 @@
 package atm.grails
 
+import com.test.atm.Currency
+import com.test.atm.ExistingBanknotes
+import groovy.transform.ToString
+
+@ToString
 class MoneyDomain {
+
     Currency currency
     int value
     int number
     static constraints = {
-        value min: 0
+        currency nullable: false
+        value min: 0, validator: { int val, MoneyDomain obj ->
+            ExistingBanknotes.assertBanknote(obj.currency, val)
+            true
+        }
         number min: 0
     }
 }
