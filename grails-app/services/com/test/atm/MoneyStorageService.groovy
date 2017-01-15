f40442d7f567f1a6954a525234dcc846ac6339f1
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-15 22:48:22 +0800

    Grails refactoring

---
+++ grails-app/services/com/test/atm/MoneyStorageService.groovy
@@ -0,0 +1,41 @@
+package com.test.atm
+
+import atm.grails.MoneyDomain
+import grails.transaction.Transactional
+import grails.validation.ValidationException
+
+//@Transactional
+class MoneyStorageService {
+    def listMoney() {
+        MoneyDomain.list()
+    }
+
+    def putMoney(Currency currencyToPut, int valueToPut, int numberToPut) {
+        def moneyToCheck = MoneyDomain.findByCurrencyAndValue(currencyToPut, valueToPut)
+        try {
+            if (moneyToCheck) {
+                moneyToCheck.number += numberToPut
+                moneyToCheck.save()(failOnError: true)
+            } else {
+                def moneyToDeposit = new MoneyDomain(currency: currencyToPut, value: valueToPut, number: numberToPut)
+                moneyToDeposit.save()(failOnError: true)
+            }
+        } catch (ValidationException e) {
+            throw new AtmStateException('CANNOT PUT')
+        }
+    }
+
+    def pollMoney(Currency currencyToPoll, int valueToPoll, int numberToPoll) {
+        def moneyToCheck = MoneyDomain.findByCurrencyAndValue(currencyToPoll, valueToPoll)
+        try {
+            if (moneyToCheck.number == numberToPoll) {
+                moneyToCheck.delete()(failOnError: true)
+            } else {
+                moneyToCheck.number -= numberToPoll
+                moneyToCheck.save()(failOnError: true)
+            }
+        } catch (ValidationException e) {
+            throw new AtmStateException('CANNOT POLL')
+        }
+    }
+}
