41a3f41c231adbb9d58b4a58eeeb8bed068b6626
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-15 15:25:48 +0800

    Grails refactoring

--- grails-app/domain/atm/grails/MoneyDomain.groovy
+++ grails-app/domain/atm/grails/MoneyDomain.groovy
@@ -2,10 +2,9 @@ package atm.grails
 
 import com.test.atm.Currency
 import com.test.atm.ExistingBanknotes
-import groovy.transform.ToString
 
-@ToString
 class MoneyDomain {
+
     Currency currency
     int value
     int number
@@ -13,7 +12,6 @@ class MoneyDomain {
         currency nullable: false
         value min: 0, validator: { int val, MoneyDomain obj ->
             ExistingBanknotes.assertBanknote(obj.currency, val)
-            true
         }
         number min: 0
     }
