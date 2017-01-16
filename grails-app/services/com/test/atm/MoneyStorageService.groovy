f343e41d84ec85af3a9f5794e4fbf0bf4ced840d
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-16 13:23:13 +0800

    Grails refactoring

--- grails-app/services/com/test/atm/MoneyStorageService.groovy
+++ grails-app/services/com/test/atm/MoneyStorageService.groovy
@@ -15,10 +15,10 @@ class MoneyStorageService {
         try {
             if (moneyToCheck) {
                 moneyToCheck.number += numberToPut
-                moneyToCheck.save()(failOnError: true)
+                moneyToCheck.save()(failOnError: true, flush: true)
             } else {
                 def moneyToDeposit = new MoneyDomain(currency: currencyToPut, value: valueToPut, number: numberToPut)
-                moneyToDeposit.save()(failOnError: true)
+                moneyToDeposit.save()(failOnError: true, flush: true)
             }
         } catch (ValidationException e) {
             throw new AtmStateException('CANNOT PUT')
@@ -29,10 +29,10 @@ class MoneyStorageService {
         def moneyToCheck = MoneyDomain.findByCurrencyAndValue(currencyToPoll, valueToPoll)
         try {
             if (moneyToCheck.number == numberToPoll) {
-                moneyToCheck.delete()(failOnError: true)
+                moneyToCheck.delete()(failOnError: true, flush: true)
             } else {
                 moneyToCheck.number -= numberToPoll
-                moneyToCheck.save()(failOnError: true)
+                moneyToCheck.save()(failOnError: true, flush: true)
             }
         } catch (ValidationException e) {
             throw new AtmStateException('CANNOT POLL')
