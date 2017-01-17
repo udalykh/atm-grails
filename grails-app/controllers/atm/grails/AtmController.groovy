85854a6fb0945774e57627a08ab8414a774a9df5
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-17 14:36:23 +0800

    Grails refactoring

--- grails-app/controllers/atm/grails/AtmController.groovy
+++ grails-app/controllers/atm/grails/AtmController.groovy
@@ -4,32 +4,46 @@ import com.test.atm.*
 import grails.converters.JSON
 
 class AtmController {
-
-    def moneyStorage
+    def moneyStorageService
 
     def index() {
+        def allMoney = moneyStorageService.listMoney()
+        [allMoney: allMoney]
     }
 
     def result() {
-        def response = [:]
+        Map<BankNote, Integer> response = [:]
         try {
             String command = params["command"]
             String currency = params["currency"]
             String value = params["value"]
             String number = params["number"]
             String amount = params["amount"]
+            def moneyFind = MoneyDomain.findAllByCurrency(Currency.getCurrency(currency))
+            def moneyMap = [:] as TreeMap
+            for (z in moneyFind) {
+                moneyMap.put(new BankNote(z.currency, z.value), z.number)
+            }
             switch (CommandType.getCommandType(command)) {
                 case CommandType.REMAININGS:
-                    response = new RequestRemainings(moneyStorage).execute()
+                    response = new RequestRemainings(new MoneyStorage(moneyMap)).execute()
+                    moneyStorageService.listMoney()
                     break
                 case CommandType.ADD:
-                    response = new DepositCommand(moneyStorage).execute(currency, value, number)
+                    response = new DepositCommand(new MoneyStorage(moneyMap)).execute(currency, value, number)
+                    moneyStorageService.putMoney(Currency.getCurrency(currency), value as Integer, number as Integer)
                     break
                 case CommandType.WITHDRAW:
-                    response = new WithdrawalCommand(moneyStorage).execute(currency, amount)
+                    response = new WithdrawalCommand(new MoneyStorage(moneyMap)).execute(currency, amount)
+                    for (m in response) {
+                        BankNote moneyNote = m.key
+                        int moneyNum = m.value
+                        moneyStorageService.pollMoney(moneyNote.currency, moneyNote.value, moneyNum)
+                    }
                     break
                 default:
-                    response = new RequestRemainings(moneyStorage).execute()
+                    response = new RequestRemainings(new MoneyStorage(moneyMap)).execute()
+                    new MoneyStorageService().listMoney()
                     break
             }
             if (!response) {
