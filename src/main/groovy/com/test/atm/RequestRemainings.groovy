bd73ac0edfdcbd6d5d814d1eca7f179f5042a923
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-21 18:02:24 +0800

    Grails refactoring

--- src/main/groovy/com/test/atm/RequestRemainings.groovy
+++ src/main/groovy/com/test/atm/RequestRemainings.groovy
@@ -1,14 +1,16 @@
 package com.test.atm
 
 import atm.grails.MoneyDomain
+import org.springframework.beans.factory.annotation.Autowired
 
 class RequestRemainings implements AtmCommand {
+    MoneyStorageService thisService
+
+    RequestRemainings(@Autowired MoneyStorageService moneyService) {
+        this.thisService = moneyService
+    }
 
     Map<BankNote, Integer> execute(String... arguments) {
-        def outMap = [:] as TreeMap
-        for (someNotes in MoneyDomain.list()) {
-            outMap.put(new BankNote(someNotes.currency, someNotes.value), someNotes.number)
-        }
-        outMap
+        thisService.listMoney()
     }
 }
