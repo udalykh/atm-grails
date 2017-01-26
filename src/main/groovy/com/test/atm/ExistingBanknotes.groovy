6098f08327df5682d9b7c88a6359ce11d1816744
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-26 23:54:08 +0800

    Grails refactoring

--- src/main/groovy/com/test/atm/ExistingBanknotes.groovy
+++ src/main/groovy/com/test/atm/ExistingBanknotes.groovy
@@ -5,18 +5,10 @@ import atm.grails.ValidNotesDomain
 class ExistingBanknotes {
     static def exBank = []
 
-    static {
-        [
-                USD: [100, 50, 20, 10, 5, 2, 1],
-                RUR: [5000, 1000, 500, 100, 50, 10],
-                CHF: [1000, 200, 100, 50, 20, 10],
-                JPY: [10000, 5000, 1000],
-                EUR: [500, 200, 100, 50, 20, 10, 5]
-        ].each {
-            key, value ->
-                value.each {
-                    exBank << new BankNote(key, it)
-                }
+    static BankNote[] getBank() {
+        def z = ValidNotesDomain.list()
+        z.each { it ->
+            exBank.add(new BankNote(it.currency, it.value))
         }
         exBank = exBank.sort().reverse()
     }
