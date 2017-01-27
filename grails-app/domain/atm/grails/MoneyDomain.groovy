72b9f8aae327f48d9bf786f9e87de60e33882c74
DUdalykh <dmitry.udalykh@yandex.ru>
2017-01-27 12:34:28 +0800

    Grails refactoring

--- grails-app/domain/atm/grails/MoneyDomain.groovy
+++ grails-app/domain/atm/grails/MoneyDomain.groovy
@@ -10,7 +10,7 @@ class MoneyDomain {
     static constraints = {
         currency nullable: false
         value min: 0, validator: { int val, MoneyDomain obj ->
-            ExistingBanknotes.assertBanknote(obj.currency, val)
+            ValidNotesDomain.findByCurrencyAndValue(obj.currency, val) as Boolean
         }
         number min: 0
     }
